8993bc79a0fd793a014cba8833108cdc6dfa4053
Peter Viechter <zelberor@schnalzerbuam.de>
2022-05-11 22:11:02 +0200

    Fix build.rs and wasm-opt

--- examples/wasm/build.rs
+++ examples/wasm/build.rs
@@ -2,6 +2,7 @@ use std::env;
 
 fn main() {
     // if WASM_BINDGEN_THREADS_MAX_MEMORY is set => set the correct linker args
+    println!("cargo:rerun-if-env-changed=WASM_BINDGEN_THREADS_MAX_MEMORY");
     if let Ok(mem_str) = env::var("WASM_BINDGEN_THREADS_MAX_MEMORY") {
         let max_memory: i32 = mem_str
             .parse()
